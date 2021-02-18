04b2b97387f61d3081190fa3457b6c799a38990a
emo4964 <79060349+emo4964@users.noreply.github.com>
2021-02-19 10:30:19 +1300

    Added a sensor for when a quiet and loud sound is sensesd

--- main.ts
+++ main.ts
@@ -2,9 +2,15 @@ input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Happy)
     soundExpression.happy.playUntilDone()
 })
+input.onSound(DetectedSound.Loud, function () {
+    basic.showIcon(IconNames.Heart)
+})
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Sad)
     soundExpression.sad.playUntilDone()
+})
+input.onSound(DetectedSound.Quiet, function () {
+    basic.showIcon(IconNames.Asleep)
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     basic.showIcon(IconNames.Asleep)
